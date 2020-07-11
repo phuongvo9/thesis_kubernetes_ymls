5697361109150a5ae7d7b8875a80aeba131b48a0
Phuong VO <47130941+phuongvo9@users.noreply.github.com>
2020-07-11 14:57:09 +0700

    Add files via upload

---
+++ delete-deployments-services.sh
@@ -0,0 +1,7 @@
+kubectl delete deployment heart-detect-api
+kubectl delete deployment node-emotion-listener
+kubectl delete deployment emotion-detect-api
+
+kubectl delete service heart-detect-api
+kubectl delete service node-emotion-listener
+kubectl delete service emotion-detect-api
