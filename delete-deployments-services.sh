b072ca417bc556ad6a1387529df9cf5182d34f0b
Phuong VO <47130941+phuongvo9@users.noreply.github.com>
2020-07-15 22:04:52 +0700

    Delete delete-deployments-services.sh

--- delete-deployments-services.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete deployment heart-detect-api
-kubectl delete deployment node-emotion-listener
-kubectl delete deployment emotion-detect-api
-
-kubectl delete service heart-detect-api
-kubectl delete service node-emotion-listener
-kubectl delete service emotion-detect-api
